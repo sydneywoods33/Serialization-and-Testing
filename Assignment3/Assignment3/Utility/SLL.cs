ef39316110c8e63976627cf92080044ea3bf523a
Sydney <147531350+sydneywoods33@users.noreply.github.com>
2024-03-25 10:32:04 -0600

    Add files via upload

---
+++ Assignment3/Assignment3/Utility/SLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class SLL
+    {
+        //•	The SLL class must implement an ILinkedListADT interface
+    }
+}
